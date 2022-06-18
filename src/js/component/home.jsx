06b4fa12514e05692a4067485f629f46d047bedd
AngelaPatarroyo <angela.patarroyo@hotmail.com>
2022-06-18 21:54:53 +0000

    traffic light build

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [Red, setRed] = useState(false);
+	const [Yellow, setYellow] = useState(false);
+	const [Green, setGreen] = useState(false);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mainbox d:flex">
+			<div className="topstick d:flex text-center mx-auto">
+
+			</div>
+			<div className="trafficlightbox d:flex text-center mx-auto">
+				<div  className="text-center mx-auto">
+					<button id="clear-red"
+					onClick={() => {
+							setRed(true);
+							setYellow(false);
+							setGreen(false);
+						}}
+						className={`${
+							Red  === true ? "redbottom" : ""
+						} rounded-circle`}>
+
+					</button>
+
+				</div>
+				<div iod="clear-yellow" className="text-center mx-auto">
+				<button   
+				onClick={() => {
+							setRed(false);
+							setYellow(true);
+							setGreen(false);
+						}}
+						className={`${
+							Yellow === true ? "yellowbottom" : ""
+						} rounded-circle`}>
+
+</button>
+
+				</div>
+				<div id= "clear-green" className="text-center mx-auto">
+				<button 
+				onClick={() => {
+					setRed(false);
+					setYellow(false);
+					setGreen(true);
+				}}
+				className={`${
+					Green === true ? "greenbottom" : ""
+				} rounded-circle`}>
+
+</button>
+
+				</div>
+
+			</div>
+
+		
+
 		</div>
 	);
 };
